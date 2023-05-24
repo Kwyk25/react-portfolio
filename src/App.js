9dbe91ad93f34201460092945ace00a12ed2b158
Kyle <kylev006@gmail.com>
2023-05-24 15:31:00 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <head>
+        <title>kyle</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <main>
+        <section>
+          <nav className="py-2  bg-green-500 flex justify-between min-w-screen">
+            <h1 className="pl-5 text-xl">Kwyk25</h1>
+            <ul className="flex item-center pr-5">
+              <li>
+                <a className=" text-white py-2 px-4" href="#">
+                  Contact
+                </a>
+              </li>
+              <li>
+                <a className="text-white py-2 px-4" href="#">
+                  About
+                </a>
+              </li>
+            </ul>
+          </nav>
+          </section>
+          <section className="bg-blue-950 max-h-screen">
+          <div className="text-center ">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl ">
+              Kyle vw
+            </h2>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
